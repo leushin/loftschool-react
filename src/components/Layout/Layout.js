fe744eb9f28436c96eb2fa3d5a85acf3483685bb
Kesha <kesha.leushin@gmail.com>
2019-01-06 02:10:58 +0300

    first step

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,29 @@
 import React, { Fragment, PureComponent } from 'react';
 import SectionTitle from '../SectionTitle';
 import './Layout.css';
+import LoginForm from '../LoginForm';
 
 class Layout extends PureComponent {
   render() {
-    return 'empty';
+    const { header, footer } = this.props;
+    return (
+        <Fragment>
+            {this.renderHeader(header)}
+            <main className="main main--with-header main--with-footer">
+                <SectionTitle className="main__title">Main</SectionTitle>
+                <LoginForm/>
+            </main>
+            {this.renderFooter(footer)}
+        </Fragment>
+    );
   }
 
   renderHeader(HeaderChild) {
-    return 'empty';
+    return <HeaderChild/>;
   }
 
   renderFooter(FooterChild) {
-    return 'empty';
+    return <FooterChild/>;
   }
 }
 
--- src/contexts/Auth/Auth.js
+++ src/contexts/Auth/Auth.js
@@ -1,14 +1,49 @@
 import React, { PureComponent } from 'react';
 
 const { Provider, Consumer: AuthConsumer } = React.createContext('');
-
-class AuthProvider extends PureComponent {
-  render() {
-    const { children } = this.props;
-    return <Provider>{children}</Provider>;
-  }
+const access = {
+    login: 'stu@dent.com',
+    pass: '123'
 }
 
-const TestProvider = Provider;
+class AuthProvider extends PureComponent {
+    state = {
+        isAuthorized: false,
+        authorizeError: null
+    }
 
-export { AuthProvider, AuthConsumer, TestProvider };
+    authorize = (login, pass) => {
+        if (login === access.login && pass === access.pass) {
+            this.setState({
+                isAuthorized: true,
+                authorizeError: null
+            });
+        } else {
+            this.setState({
+                isAuthorized: false,
+                authorizeError: 'Email или пароль введён не верно'
+            });
+        }
+    }
+
+    logOut = () => {
+        this.setState({
+            isAuthorized: false,
+            authorizeError: null
+        });
+    }
+
+    render() {
+        const { children } = this.props;
+        const data = {
+            isAuthorized: this.state.isAuthorized,
+            authorizeError: this.state.authorizeError,
+            authorize: this.authorize,
+            logOut: this.logOut
+        }
+
+        return <Provider value={data}>{children}</Provider>;
+    }
+}
+
+export { AuthProvider, AuthConsumer };
